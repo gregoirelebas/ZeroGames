772b1acfa6c27459d1d2d8b87b9b1f5eab8331d7
Grégoire Lebas <gregoire.lebas@gmail.com>
2021-03-29 17:10:48 +0200

    Allow to add and remove ingredients.

---
+++ Assets/Scripts/Recipe/IngredientSlot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngredientSlot : MonoBehaviour
+{
+	[SerializeField] private TextMeshProUGUI nameText = null;
+	[SerializeField] private Button deleteBtn = null;
+
+	public void SetIngredient(RecipeEditor editor, string name)
+	{
+		nameText.text = name;
+		deleteBtn.onClick.AddListener(() =>
+		{
+			editor.RemoveIngredient(this);
+			Destroy(gameObject);
+		});
+	}
+}
--- Assets/Scripts/Recipe/RecipeEditor.cs
+++ Assets/Scripts/Recipe/RecipeEditor.cs
@@ -11,7 +11,18 @@ public class RecipeEditor : MonoBehaviour
 	[SerializeField] private TextMeshProUGUI interactText = null;
 	[SerializeField] private GameObject panel = null;
 
+	[Header("Ingredients")]
+	[SerializeField] private GameObject ingredientSlotPrefab = null;
+	[SerializeField] private Transform ingredientListContainer = null;
+	[SerializeField] private TMP_InputField ingredientField = null;
+
+	[Header("Recipes")]
+	[SerializeField] private GameObject recipeSlotPrefab = null;
+	[SerializeField] private Transform recipeGridContainer = null;
+
 	private Player player = null;
+
+	private List<IngredientSlot> ingredientSlots = new List<IngredientSlot>();
 
 	private void OnEnable()
 	{
@@ -26,6 +37,16 @@ public class RecipeEditor : MonoBehaviour
 			player.SetMove(false);
 			interactText.text = "";
 			panel.SetActive(true);
+
+			for (int i = 0; i < ingredientListContainer.childCount; i++)
+			{
+				Destroy(ingredientListContainer.GetChild(i).gameObject);
+			}
+
+			for (int i = 0; i < recipeGridContainer.childCount; i++)
+			{
+				Destroy(recipeGridContainer.GetChild(i).gameObject);
+			}
 		}
 	}
 
@@ -39,12 +60,31 @@ public class RecipeEditor : MonoBehaviour
 	{
 		this.player = player;
 		interactText.text = "Search recipes";
-		
+
 	}
 
 	private void HideInteractionText()
 	{
 		player = null;
 		interactText.text = "";
+	}
+
+	public void AddIngredient()
+	{
+		if (ingredientField.text != "")
+		{
+			GameObject go = Instantiate(ingredientSlotPrefab, ingredientListContainer);
+			IngredientSlot newSlot = go.GetComponent<IngredientSlot>();
+
+			newSlot.SetIngredient(this, ingredientField.text);
+			ingredientSlots.Add(newSlot);
+
+			ingredientField.text = "";
+		}
+	}
+
+	public void RemoveIngredient(IngredientSlot toRemove)
+	{
+		ingredientSlots.Remove(toRemove);
 	}
 }
