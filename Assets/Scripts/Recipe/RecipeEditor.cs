1b12189d8f01ef47ca11632483783bc17972309e
Grégoire Lebas <gregoire.lebas@gmail.com>
2021-03-29 16:53:08 +0200

    Display recipe with player interaction.

--- Assets/Scripts/Recipe/RecipeEditor.cs
+++ Assets/Scripts/Recipe/RecipeEditor.cs
@@ -1,8 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RecipeEditor : MonoBehaviour
 {
-    
+	[Header("Interaction")]
+	[SerializeField] private PlayerAreaTrigger trigger = null;
+	[SerializeField] private TextMeshProUGUI interactText = null;
+	[SerializeField] private GameObject panel = null;
+
+	private Player player = null;
+
+	private void OnEnable()
+	{
+		trigger.OnPlayerEnter += SetInteractionText;
+		trigger.OnPlayerExit += HideInteractionText;
+	}
+
+	private void Update()
+	{
+		if (player != null && Keyboard.current.eKey.wasPressedThisFrame)
+		{
+			player.SetMove(false);
+			interactText.text = "";
+			panel.SetActive(true);
+		}
+	}
+
+	private void OnDisable()
+	{
+		trigger.OnPlayerEnter -= SetInteractionText;
+		trigger.OnPlayerExit -= HideInteractionText;
+	}
+
+	private void SetInteractionText(Player player)
+	{
+		this.player = player;
+		interactText.text = "Search recipes";
+		
+	}
+
+	private void HideInteractionText()
+	{
+		player = null;
+		interactText.text = "";
+	}
 }
