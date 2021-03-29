11bbd513e87ec013597d0f6576754e6d8302174b
Grégoire Lebas <gregoire.lebas@gmail.com>
2021-03-29 09:51:22 +0200

    Move player with mouse. Also moved Scripts folder out of Scenes?

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+	private NavMeshAgent agent = null;
+	private Camera mainCamera = null;
+
+	private void Awake()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		mainCamera = Camera.main; //DEBUG
+	}
+
+	private void Update()
+	{
+		if (Mouse.current.leftButton.isPressed)
+		{
+			Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+			Ray ray = mainCamera.ScreenPointToRay(mousePosition);
+			if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+			{
+				agent.SetDestination(hit.point);
+			}			
+		}
+	}
+
+	//DEBUG
+	private void OnDrawGizmos()
+	{
+		if (agent != null)
+		{
+			Gizmos.color = Color.blue;
+			Gizmos.DrawSphere(agent.destination, 0.5f);
+		}
+	}
+}
