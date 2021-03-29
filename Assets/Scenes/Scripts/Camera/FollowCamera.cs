accc458257e0e7b65557738a00d42652d04bb1f9
Grégoire Lebas <gregoire.lebas@gmail.com>
2021-03-29 08:50:56 +0200

    Move the camera long with the player.

---
+++ Assets/Scenes/Scripts/Camera/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+	[SerializeField] private Transform target = null;
+
+	private Vector3 offset = Vector3.zero;
+
+	private void Awake()
+	{
+		offset = target.position - transform.position;
+	}
+
+	private void LateUpdate()
+	{
+		transform.LookAt(target);
+		transform.position = target.position - offset;
+	}
+}
